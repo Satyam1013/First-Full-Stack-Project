180b2fbda9b5e03709e7c9abe7988cca0d04f5a5
Satyam Banwale <satyambanwale786@gmail.com>
2023-04-07 14:49:54 +0530

    done 1st full stack app

--- frontend/src/components/Notes.jsx
+++ frontend/src/components/Notes.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 const Notes = () => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [sub, setSub] = useState("");
   const [notes, setNotes] = useState([]);
+  console.log(title, desc, sub);
+  console.log(notes);
   const getData = async () => {
     try {
       const res = await fetch(
@@ -19,6 +24,26 @@ const Notes = () => {
       console.log(err);
     }
   };
+  const handleAdd = () => {
+    const payload = {
+      title,
+      description: desc,
+      subject: sub,
+    };
+
+    fetch("https://lime-encouraging-walkingstick.cyclic.app/notes/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   const handleDelete = async (x) => {
     const arr = notes.filter((el) => el._id !== x._id);
     setNotes(arr);
@@ -37,36 +62,63 @@ const Notes = () => {
   };
   useEffect(() => {
     getData();
-  }, []);
+  }, [title, desc, sub]);
   return (
-    <div
-      style={{
-        margin: "auto",
-        marginTop: "50px",
-        display: "grid",
-        gridTemplateColumns: "repeat(2,1fr)",
-        gap: "30px",
-      }}
-    >
-      {notes.map((el) => (
-        <div
-          style={{
-            margin: "auto",
-            border: "solid 1px black",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            width: "300px",
-          }}
-          key={el._id}
-        >
-          <h1>{el.title}</h1>
-          <p>{el.description}</p>
-          <h2>{el.subject}</h2>
-          <button onClick={() => handleDelete(el)}>Delete</button>
-        </div>
-      ))}
-    </div>
+    <>
+      <div
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          justifyContent: "center",
+          gap: "30px",
+        }}
+      >
+        <input
+          value={title}
+          placeholder="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          value={desc}
+          placeholder="description"
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <input
+          value={sub}
+          placeholder="subject"
+          onChange={(e) => setSub(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div
+        style={{
+          margin: "auto",
+          marginTop: "50px",
+          display: "grid",
+          gridTemplateColumns: "repeat(2,1fr)",
+          gap: "30px",
+        }}
+      >
+        {notes.map((el) => (
+          <div
+            style={{
+              margin: "auto",
+              border: "solid 1px black",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "300px",
+            }}
+            key={el._id}
+          >
+            <h1>{el.title}</h1>
+            <p>{el.description}</p>
+            <h2>{el.subject}</h2>
+            <button onClick={() => handleDelete(el)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
